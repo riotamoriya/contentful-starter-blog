2e328a55842e3b5c1bcc1757d1d93ef0e22528b4
riotamoriya <riotamoriya@gmail.com>
2023-09-25 14:12:58 +0900

    first commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,19 +5,16 @@ import './global.css'
 import Seo from './seo'
 import Navigation from './navigation'
 import Footer from './footer'
-class Template extends React.Component {
-  render() {
-    const { children } = this.props
 
-    return (
-      <>
-        <Seo />
-        <Navigation />
-        <main>{children}</main>
-        <Footer />
-      </>
-    )
-  }
+const Template = ({ children }) => {
+  return (
+    <>
+      <Seo />
+      <Navigation />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
 }
 
 export default Template
--- src/pages/index copy.js
+++ src/pages/index copy.js
@@ -6,20 +6,22 @@ import Layout from '../components/layout'
 import Hero from '../components/hero'
 import ArticlePreview from '../components/article-preview'
 
-const RootIndex = ({ data, location }) => {
-  const posts = get(data, 'allContentfulBlogPost.nodes')
-  const [author] = get(data, 'allContentfulPerson.nodes')
+class RootIndex extends React.Component {
+  render() {
+    const posts = get(this, 'props.data.allContentfulBlogPost.nodes')
+    const [author] = get(this, 'props.data.allContentfulPerson.nodes')
 
-  return (
-    <Layout location={location}>
-      <Hero
-        image={author.heroImage.gatsbyImage}
-        title={author.name}
-        content={author.shortBio}
-      />
-      <ArticlePreview posts={posts} />
-    </Layout>
-  )
+    return (
+      <Layout location={this.props.location}>
+        <Hero
+          image={author.heroImage.gatsbyImage}
+          title={author.name}
+          content={author.shortBio}
+        />
+        {/* <ArticlePreview posts={posts} /> */}
+      </Layout>
+    )
+  }
 }
 
 export default RootIndex
